fb9e60206ad18648347da2c0343022c340f3550e
HoshinoSuzumi <master@uniiem.com>
2024-11-15 17:13:11 +0800

    ➕ add tailwind

--- app.vue
+++ app.vue
@@ -1,3 +1,3 @@
 <template>
-  <HelloWorld />
+  <RayButton>Click me!</RayButton>
 </template>
---
+++ components/ray/Button.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <button class="bg-cyan-200 rounded-lg border border-neutral-50 px-2 py-1">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+
+</style>
